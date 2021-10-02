e42f112c3be2ab0b823766d02e4d94770a970981
Kasper Garnaes <kasper.garnaes@gmail.com>
2021-10-02 15:58:42 +0200

    Remove Drupal Core projects from System Status response

--- src/SiteStatus/SiteData.php
+++ src/SiteStatus/SiteData.php
@@ -22,10 +22,21 @@ class SiteData
             return (array) $this->data->getData()->system_status->{$group};
         }, $groups));
 
-        $projectVersionsMap = array_map(static function (object $project) {
+        $projectVersionsMap = array_map(function (object $project) {
             return $project->version;
         }, $projects);
 
-        return \array_filter($projectVersionsMap);
+        // System status module may report modules which are a part of Drupal Core and thus not projects in their own
+        // sense. Remove these based an the heuristic that core modules will have the same version as Drupal Core.
+        $coreVersion = $projectVersionsMap["drupal"];
+        $filteredVersionMap = array_filter(
+            $projectVersionsMap,
+            function ($version, $project) use ($coreVersion) {
+                return $project === "drupal" || $version !== $coreVersion;
+            },
+            ARRAY_FILTER_USE_BOTH
+        );
+
+        return array_filter($filteredVersionMap);
     }
 }
