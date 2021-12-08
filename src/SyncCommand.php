400b12f9826ec4f2f7d33395ad1f6268a9165b18
Kasper Garnaes <kasper.garnaes@gmail.com>
2021-12-08 12:35:43 +0100

    Remove unused import

--- src/SyncCommand.php
+++ src/SyncCommand.php
@@ -10,7 +10,6 @@ use DrupalSecurityJira\SystemStatus\Fetcher;
 use Reload\JiraSecurityIssue;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpClient\HttpClient;
