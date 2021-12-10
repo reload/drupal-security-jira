57f663426f6b81d8f39ef3613f9f55aebeb0b482
Kasper Garnaes <kasper.garnaes@gmail.com>
2021-12-10 11:52:44 +0100

    Denormalize secure versions
    This ensures compatibility with current integration.

--- src/DrupalOrg/VersionGroup.php
+++ src/DrupalOrg/VersionGroup.php
@@ -24,6 +24,15 @@ class VersionGroup
         return preg_replace('/^\d\.(\d\.)?x\-/', '', $version);
     }
 
+    private function denormalizeVersion(string $normalizedVersion, string $referenceVersion): string
+    {
+        $normalizedReference = $this->normalizeVersion($referenceVersion);
+        // Assume that a normalized version is a stripped down version of a
+        // reference and we can denormalize any version by replacing any version
+        // into a reference.
+        return str_replace($normalizedReference, $normalizedVersion, $referenceVersion);
+    }
+
     public function getNextVersion(string $currentVersion): string
     {
         $versions = array_combine($this->versions, $this->versions);
@@ -45,6 +54,7 @@ class VersionGroup
         if (!is_string($nextVersion)) {
             throw new \RuntimeException("Unexpected value for next version $nextVersion.");
         }
-        return $nextVersion;
+
+        return $this->denormalizeVersion($nextVersion, $currentVersion);
     }
 }
